51ffd166b9a3e0675e42c770ce2439b9e840dfc8
davaloslm <davaloslm@hotmail.com.ar>
2022-12-25 15:40:01 -0300

    Add CheckUser Servlet

---
+++ src/java/CheckUserServlet.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Leo
+ */
+@WebServlet(urlPatterns = {"/CheckUserServlet"})
+public class CheckUserServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            Persistencia base = new Persistencia();
+
+            ResultSet rs = base.consultaSQL("select * from usuarios where usuario ="
+                    + "'" + request.getParameter("email") + "'" + "and clave = "
+                    + "'" + request.getParameter("password") + "'");
+
+            while (rs.next()) {
+                out.println(rs.getString("usuario"));
+                out.println(rs.getString("nombreyapellido") + "<BR>");
+            }
+
+            if (rs.first() == false) {
+                out.println("No hay usuarios que coincidan con la busqueda");
+
+                out.println("<h1>Servlet CheckUserServlet at " + request.getContextPath() + "</h1>");
+                out.println("<h1>El usuario ingresado es: " + request.getParameter("email") + "</h1>");
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CheckUserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+
+    
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        /**
+         * Handles the HTTP <code>GET</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doGet
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo
+        
+            () {
+        return "Short description";
+        }// </editor-fold>
+ 
+}
